57aae1f42bdc7b26033176d6be1c3cf413a5fe51
ayomanwhathuhhuh <123750756+ayomanwhathuhhuh@users.noreply.github.com>
2024-03-14 13:36:41 -0400

    unfinished

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ let header = document.getElementById("header"); // ignore the ugly "getElementBy
 if (window.innerWidth > 768) {
     primarynavbarmobile.style.display = "none";
     primarynavbar.style.flexDirection = "row";
-    simplifyqueryselectionall((element) => {element.style.opacity = "1"});
+    simplifyqueryselectionall((element) => {element.style.opacity = "1"; element.style.pointerEvents = "auto";});
 } else {
     primarynavbarmobile.style.display = "flex";
     primarynavbar.style.flexDirection = "column";
-    simplifyqueryselectionall((element) => {element.style.opacity = "0"});
+    simplifyqueryselectionall((element) => {element.style.opacity = "0"; element.style.pointerEvents = "none";});
 }
 // run on window resize
 window.addEventListener("resize", (event) => {
@@ -35,13 +35,13 @@ window.addEventListener("resize", (event) => {
         primarynavbarmobile.style.display = "none";
         header.style.height = "3rem"; // reset navbar height
         primarynavbar.style.flexDirection = "row"; // and direction
-        simplifyqueryselectionall((element) => {element.style.opacity = "1"; element.style.margin = "0.5rem";}) // default size and stuff
+        simplifyqueryselectionall((element) => {element.style.opacity = "1"; element.style.margin = "0.5rem"; element.style.pointerEvents = "auto";}) // default size and stuff
         navbar = true // navbar fully reset probably
     } else {
         primarynavbarmobile.style.display = "flex"; // turn on the navbar button
         primarynavbar.style.flexDirection = "column"; // switch navbar direction
         if (navbar == true) { // added if statement because the elements kept going poof while resizing the window
-            simplifyqueryselectionall((element) => {element.style.opacity = "0"; element.style.margin = "2rem";}) // turn off the navbar elements
+            simplifyqueryselectionall((element) => {element.style.opacity = "0"; element.style.margin = "2rem"; element.style.pointerEvents = "none";}) // turn off the navbar elements
         }
     }
 });
@@ -49,11 +49,11 @@ let navbar = true;
 function navbarmenuclick() {
     if (navbar == true) {
         header.style.height = "100vh";
-        simplifyqueryselectionall((element) => {element.style.opacity = "1"}) // expanded
+        simplifyqueryselectionall((element) => {element.style.opacity = "1"; element.style.pointerEvents = "auto";}) // expanded
         navbar = false // expanded navbar
     } else { // run when either the window is bigger then 768 pixels OR the navbar is collapsing (idk how to spell)
         header.style.height = "3rem";
-        simplifyqueryselectionall((element) => {element.style.opacity = "0";}) // turn off the navbar elements
+        simplifyqueryselectionall((element) => {element.style.opacity = "0"; element.style.pointerEvents = "none";}) // turn off the navbar elements
         navbar = true // navbar back to default
     }
 }
