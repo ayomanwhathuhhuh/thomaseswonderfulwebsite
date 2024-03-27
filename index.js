5ab26de7dd8a44f5a1e2795526a4137f05b14bf5
thomas licata <licata.thomas@students.gcecnj.org>
2024-03-27 19:57:51 -0400

    Done? again?

--- index.js
+++ index.js
@@ -66,35 +66,35 @@ function navbarmenuclick() {
 
 // others
 // gotta love javascript and how cursed it is
-doxerboxer.innerText = "nothing... 1x";
-if (location.protocol !== 'file:') {
-    const getJSON = async url => {
-        const response = await fetch(url);
-        return response.json();
-    }
-    let doxerboxer = document.getElementById("doxerboxer");
-    let doxarray = []
-    doxerboxer.innerText = "nothing...";
-    getJSON("https://geolocation-db.com/json/")
-        .then(data => { // first lookup for basic information not very accurate
-            doxarray[0] = data; // make sure to store that ipv4 address
-            getJSON(`https://ezcmd.com/apps/api_ezip_locator/lookup/GUEST_USER/-1/${data.IPv4}`) // use previous data (ip address) to find exact details
-                .then(data2 => {
-                    doxarray[1] = data2; // new exact details i.e hometown state city
-                    datafinish(); // call the final function since all of the information is now here
-                })
-        })
-        doxerboxer.innerText = "nothing... still";
-    function datafinish() {
-        let short = doxarray[1].nearest_postal_code_info; // to make the array readable
-        let firstarray = [short.country_name, short.country_code, short.postal_code, short.place_name, short.state, short.province, "Latitude " + short.lat + " Degrees", "Longitude " + short.lon + " Degrees"];
-        let intostring = ""
-        firstarray.map((value) => { // get all values from the array and put them all into a string (seperated with line breaks)
-            intostring += value + "\n"
-        })
-        doxerboxer.innerText = "IPv4: " + doxarray[0].IPv4 + "\n" + intostring; // get all information and set the div text
-    }
-} else {
-    console.log("local file don't dox");
-    doxerboxer.innerText = "currently on local file no dox";
-}
+doxerboxer.innerText = "nothing...";
+// if (location.protocol !== 'file:') {
+//     const getJSON = async url => {
+//         const response = await fetch(url);
+//         return response.json();
+//     }
+//     let doxerboxer = document.getElementById("doxerboxer");
+//     let doxarray = []
+//     doxerboxer.innerText = "nothing...";
+//     getJSON("https://geolocation-db.com/json/")
+//         .then(data => { // first lookup for basic information not very accurate
+//             doxarray[0] = data; // make sure to store that ipv4 address
+//             getJSON(`https://ezcmd.com/apps/api_ezip_locator/lookup/GUEST_USER/-1/${data.IPv4}`) // use previous data (ip address) to find exact details
+//                 .then(data2 => {
+//                     doxarray[1] = data2; // new exact details i.e hometown state city
+//                     datafinish(); // call the final function since all of the information is now here
+//                 })
+//         })
+//         doxerboxer.innerText = "nothing... still";
+//     function datafinish() {
+//         let short = doxarray[1].nearest_postal_code_info; // to make the array readable
+//         let firstarray = [short.country_name, short.country_code, short.postal_code, short.place_name, short.state, short.province, "Latitude " + short.lat + " Degrees", "Longitude " + short.lon + " Degrees"];
+//         let intostring = ""
+//         firstarray.map((value) => { // get all values from the array and put them all into a string (seperated with line breaks)
+//             intostring += value + "\n"
+//         })
+//         doxerboxer.innerText = "IPv4: " + doxarray[0].IPv4 + "\n" + intostring; // get all information and set the div text
+//     }
+// } else {
+//     console.log("local file don't dox");
+//     doxerboxer.innerText = "currently on local file no dox";
+// }
