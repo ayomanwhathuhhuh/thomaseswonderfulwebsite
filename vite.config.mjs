4003197ecf75f381aeb2aca1dfaa9ec04ba83827
thomas licata <licata.thomas@students.gcecnj.org>
2024-03-29 15:25:48 -0400

    Hopefully fixes the "The CJS build of Vite's Node API is deprecated" warning

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 export default defineConfig({
     base: '/thomaseswonderfulwebsite/', // best not to ask why this works i really do not know
     plugins: [],
-});
+})
