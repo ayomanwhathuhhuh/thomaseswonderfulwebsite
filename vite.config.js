489cca8f6e03a34c4551f20c02d2f9a4a4cfa0e6
thomas licata <licata.thomas@students.gcecnj.org>
2024-03-29 15:10:38 -0400

    I DID IT YESSS FINALLY

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-    base: '/thomaseswonderfulwebsite/',
+    base: '/thomaseswonderfulwebsite/', // best not to ask why this works i really do not know
     plugins: [],
 });
