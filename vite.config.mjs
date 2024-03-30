17a8f817567cf230150620ccaf4afc2a82267f09
ayomanwhathuhhuh <123750756+ayomanwhathuhhuh@users.noreply.github.com>
2024-03-30 15:59:01 -0400

    Maybe?

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 export default defineConfig({
     base: '/thomaseswonderfulwebsite/', // best not to ask why this works i really do not know
     plugins: [],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            nested: resolve(__dirname, 'nested/index.html'),
+          },
+        },
+      },
 })
+
