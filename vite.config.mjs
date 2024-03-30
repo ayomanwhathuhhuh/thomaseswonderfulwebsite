f4df089f406eed11bf5202bd5950c19690c5e6cc
ayomanwhathuhhuh <123750756+ayomanwhathuhhuh@users.noreply.github.com>
2024-03-30 16:16:28 -0400

    early easter...

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,10 +4,10 @@ export default defineConfig({
     base: '/thomaseswonderfulwebsite/', // best not to ask why this works i really do not know
     plugins: [],
     build: {
-        rollupOptions: {
+        rollupOptions: { // dawg this gets worse the more i know
           input: {
             main: resolve(__dirname, 'index.html'),
-            nested: resolve(__dirname, 'nested/index.html'),
+            otherpage: resolve(__dirname, 'otherpage.html'),
           },
         },
       },
